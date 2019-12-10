0ee58cb0082b0e7e8e74cd21c9c9acfdfa59c7cf
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-12-10 06:56:28 -0800

    Update Program.cs

--- NetCore.PDF.to.PNG.Single.Example/NetCore.PDF.to.PNG.Single.Example/Program.cs
+++ NetCore.PDF.to.PNG.Single.Example/NetCore.PDF.to.PNG.Single.Example/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Cloudmersive.APIClient.NETCore.DocumentAndDataConvert.Api;
+using Cloudmersive.APIClient.NETCore.DocumentAndDataConvert.Client;
+using System;
 
 namespace NetCore.PDF.to.PNG.Single.Example
 {
@@ -6,7 +8,19 @@ namespace NetCore.PDF.to.PNG.Single.Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting...");
+
+            // Configure API key authorization: Apikey
+            Configuration.Default.AddApiKey("Apikey", "YOUR_API_KEY");
+
+
+
+            var apiInstance = new ConvertDocumentApi();
+            var inputFile = new System.IO.FileStream("C:\\temp\\inputfile", System.IO.FileMode.Open); // System.IO.Stream | Input file to perform the operation on.
+
+            byte[] result = apiInstance.ConvertDocumentPdfToPngSingle(inputFile);
+
+            Console.WriteLine("Done.");
         }
     }
 }
